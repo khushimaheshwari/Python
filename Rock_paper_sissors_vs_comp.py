ba2cf7b824f6ca21464b668139ce40e824cde71f
skagnihotri <skagnihotri1@gmail.com>
2020-10-04 15:54:45 +0530

    changes made in game

--- Rock_paper_sissors_vs_comp.py
+++ Rock_paper_sissors_vs_comp.py
@@ -1,45 +1,51 @@
 import random
 #TO USE RANDOM
 
-print("...rock...\n")
-print("...paper...\n")
-print("...sissors...\n")
 
-#ENTER CHOICES
-player = input("Enter player 1's choice: ")
+def game(player) :
 
-num = random.randint(0,2)
+	num = random.randint(0,2)
+	if num == 0:
+		computer = "rock"
+	elif num == 1:
+		computer = "paper"
+	else:
+		computer = "sissors"
 
-if num == 0:
-	computer = "rock"
-elif num == 1:
-	computer = "paper"
-else:
-	computer = "sissors"
-print("computer chooses: " + computer)
+	print("computer chooses: " + computer)
 
-#DECLRARING WINNER
+	#DECLRARING WINNER
+	if player == computer:
+			print("TRY AGAIN, ITS A TIE!")
 
-if player == computer:
-		print("TRY AGAIN, ITS A TIE!")
+	elif player == "rock":
+		if computer == 'paper':
+			print("computer wins!")
+		elif computer == 'sissors':
+			print("player wins!")
 
-elif player == "rock":
-	if computer == 'paper':
-		print("computer wins!")
-	elif computer == 'sissors':
-		print("player wins!")
+	elif player == "paper":
+		if computer == 'rock':
+			print("player wins!")
+		elif computer == 'sissors':
+			print("computer wins!")	
+	
+	elif player == 'sissors':
+		if computer == 'sissors':
+			print("TRY AGAIN!")
+		elif computer == 'rock':
+			print("computer wins!")
+		elif computer == 'paper':
+			print("player wins!")	
+	else:
+		print("enter choice again!")
 
-elif player == "paper":
-	if computer == 'rock':
-		print("player wins!")
-	elif computer == 'sissors':
-		print("computer wins!")	
-elif player == 'sissors':
-	if computer == 'sissors':
-		print("TRY AGAIN!")
-	elif computer == 'rock':
-		print("computer wins!")
-	elif computer == 'paper':
-		print("player wins!")	
-else:
-	print("enter choice again!")		
+
+if __name__ == '__main__':
+	print("...rock...")
+	print("...paper...")
+	print("...sissors...")
+
+	#ENTER CHOICES
+	player = input("Enter player 1's choice: ")
+	game(player.lower())
